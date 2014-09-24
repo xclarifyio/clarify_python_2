c79956cc736d922efa1d1c466467479c25bc10a2
Paul Murphy <paul@clarify.io>
2014-09-24 15:28:34 -0500

    Fixed some pep8 grumbles.

--- tests.dev/common.py
+++ tests.dev/common.py
@@ -1,25 +1,26 @@
-##
-##  Functions used by multiple tests.
-##
+#
+#  Functions used by multiple tests.
+#
 
 import sys
 sys.path.append('..')
 from clarify_python_2 import clarify
 
+
 def bundle_list_map(func):
     """Execute func on every bundle."""
     has_next = True
-    next_href = None # if None, retrieves first page
+    next_href = None  # if None, retrieves first page
 
     while has_next:
         # Get a page and perform the requested function.
-        bl = clarify.get_bundle_list(next_href)
-        for i in bl['_links']['items']:
+        bundle_list = clarify.get_bundle_list(next_href)
+        for i in bundle_list['_links']['items']:
             href = i['href']
             func(href)
         # Check for following page.
         next_href = None
-        if bl['_links'].has_key('next'):
-            next_href = bl['_links']['next']['href']
-        if next_href == None:
-                has_next = False
+        if 'next' in bundle_list['_links']:
+            next_href = bundle_list['_links']['next']['href']
+        if next_href is None:
+            has_next = False
